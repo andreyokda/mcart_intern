6162fb10cd4ed163080c98f2e62d2de80b6ffcc5
Andrey <andreyokda@yandex.ru>
2025-02-26 23:04:24 +0300

    Задача № 184421 + 5. Размещение компонентов

--- kabinet-pokupatelya/index.php
+++ kabinet-pokupatelya/index.php
@@ -1,4 +1,28 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Кабинет покупателя");
-?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.profile",
+	"",
+	Array(
+		"CHECK_RIGHTS" => "N",
+		"SEND_INFO" => "N",
+		"SET_TITLE" => "Y",
+		"USER_PROPERTY" => array(),
+		"USER_PROPERTY_NAME" => ""
+	)
+);?><br>
+ <br>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:subscribe.form", 
+	".default", 
+	array(
+		"CACHE_TIME" => "3600",
+		"CACHE_TYPE" => "A",
+		"PAGE" => "#SITE_DIR#about/subscr_edit.php",
+		"SHOW_HIDDEN" => "Y",
+		"USE_PERSONALIZATION" => "Y",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
