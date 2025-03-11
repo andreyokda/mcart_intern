40fc70fa068b8563cd3e85576630f741d7031aeb
Andrey <andreyokda@yandex.ru>
2025-03-11 21:04:08 +0300

    Задача № 184423 + 1. Внедрение шаблона. Правки lang

--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -3,7 +3,7 @@ IncludeTemplateLangFile(__FILE__);
 ?>
 
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="<?= LANGUAGE_ID; ?>">
 
 <head>
   <?$APPLICATION->ShowHead();?>
@@ -68,17 +68,43 @@ IncludeTemplateLangFile(__FILE__);
         <div class="row align-items-center">
           <div class="col-6 col-md-6">
             <p class="mb-0">
-              <a href="#" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span
-                  class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
-              <a href="#"><span class="text-black fl-bigmug-line-email64"></span> <span
-                  class="d-none d-md-inline-block ml-2">info@domain.com</span></a>
+                  <?$APPLICATION->IncludeComponent(
+                    "bitrix:main.include", 
+                    ".default", 
+                    array(
+                      "AREA_FILE_RECURSIVE" => "Y",
+                      "AREA_FILE_SHOW" => "sect",
+                      "AREA_FILE_SUFFIX" => "tel",
+                      "EDIT_TEMPLATE" => "",
+                      "COMPONENT_TEMPLATE" => ".default"
+                    ),
+                    false
+                  );?>
+                  <?$APPLICATION->IncludeComponent(
+                    "bitrix:main.include",
+                    "",
+                    Array(
+                      "AREA_FILE_RECURSIVE" => "Y",
+                      "AREA_FILE_SHOW" => "sect",
+                      "AREA_FILE_SUFFIX" => "email",
+                      "EDIT_TEMPLATE" => ""
+                    )
+                  );?>
             </p>
           </div>
           <div class="col-6 col-md-6 text-right">
-            <a href="#" class="mr-3"><span class="text-black icon-facebook"></span></a>
-            <a href="#" class="mr-3"><span class="text-black icon-twitter"></span></a>
-            <a href="#" class="mr-0"><span class="text-black icon-linkedin"></span></a>
+          <?$APPLICATION->IncludeComponent(
+            "bitrix:main.include",
+            "",
+            Array(
+              "AREA_FILE_RECURSIVE" => "Y",
+              "AREA_FILE_SHOW" => "sect",
+              "AREA_FILE_SUFFIX" => "icon",
+              "EDIT_TEMPLATE" => ""
+            )
+          );?>
           </div>
+          
         </div>
       </div>
 
@@ -87,51 +113,74 @@ IncludeTemplateLangFile(__FILE__);
       <div class="container py-1">
         <div class="row align-items-center">
           <div class="col-8 col-md-8 col-lg-4">
-            <h1 class=""><a href="index.php" class="h5 text-uppercase text-black"><strong>HomeSpace<span
-                    class="text-danger">.</span></strong></a></h1>
+          <?$APPLICATION->IncludeComponent(
+            "bitrix:main.include",
+            ".default",
+            Array(
+              "AREA_FILE_RECURSIVE" => "Y",
+              "AREA_FILE_SHOW" => "sect",
+              "AREA_FILE_SUFFIX" => "logo",
+              "COMPONENT_TEMPLATE" => ".default",
+              "EDIT_TEMPLATE" => ""
+            )
+          );?>
           </div>
           <div class="col-4 col-md-4 col-lg-8">
-            <nav class="site-navigation text-right text-md-right" role="navigation">
+          <nav class="site-navigation text-right text-md-right" role="navigation">
 
-              <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
-                  class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+            <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
+                class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
 
-              <ul class="site-menu js-clone-nav d-none d-lg-block">
+            <ul class="site-menu js-clone-nav d-none d-lg-block">
+              <li class="active">
+                <a href="index.html">Home</a>
+              </li>
+              <li class="has-children">
+                <a href="properties.html">Properties</a>
+                <ul class="dropdown">
+                  <li><a href="#">Buy</a></li>
+                  <li><a href="#">Rent</a></li>
+                  <li><a href="#">Lease</a></li>
+                  <li class="has-children">
+                    <a href="#">Menu</a>
+                    <ul class="dropdown">
+                      <li><a href="#">Menu One</a></li>
+                      <li><a href="#">Menu Two</a></li>
+                      <li><a href="#">Menu Three</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="blog.html">Blog</a></li>
+              <li><a href="about.html">About</a></li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
+          </nav>
 
-                <li class="active"><a href="/">Главная</a></li>
+            <?$APPLICATION->IncludeComponent(
+              "bitrix:menu", 
+              "horizontal_multilevel", 
+              array(
+                "ALLOW_MULTI_SELECT" => "N",
+                "CHILD_MENU_TYPE" => "left",
+                "DELAY" => "N",
+                "MAX_LEVEL" => "4",
+                "MENU_CACHE_GET_VARS" => array(
+                ),
+                "MENU_CACHE_TIME" => "3600",
+                "MENU_CACHE_TYPE" => "N",
+                "MENU_CACHE_USE_GROUPS" => "Y",
+                "ROOT_MENU_TYPE" => "top",
+                "USE_EXT" => "N",
+                "COMPONENT_TEMPLATE" => "horizontal_multilevel",
+                "MENU_THEME" => "site"
+              ),
+              false
+            );?>
 
-                <li><a href="/obyavleniya">Объявления</a></li>
-
-                <li class="has-children">
-                  <a href="/o-servise">О сервисе</a>
-                  <ul class="dropdown">
-                    <li><a href="/o-servise/vakansii">Вакансии</a></li>
-                    <li class="has-children">
-                      <a href="/o-servise/kontakty">Контакты</a>
-                      <ul class="dropdown">
-                        <li><a href="/o-servise/kontakty/obratnaya-svyaz.php">Обратная связь</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="/o-servise/novosti">Новости</a></li>
-                    <li><a href="#">Агенты</a></li>
-                  </ul>
-                </li>
-
-                <li class="has-children"><a href="/kabinet-prodavtsa">Кабинет продавца</a>
-                  <ul class="dropdown">
-                    <li><a href="/kabinet-prodavtsa/moi-obyavleniya">Мои объявления</a></li>
-                  </ul>
-                </li>
-
-                <li class="has-children"><a href="/kabinet-pokupatelya">Кабинет покупателя</a>
-                  <ul class="dropdown">
-                    <li><a href="/kabinet-pokupatelya/izbrannoe">Избранное</a></li>
-                  </ul>
-                </li>
-
-              </ul>
-            </nav>
           </div>
+
+          
 
 
         </div>
---
+++ novaya-stranitsa.php
@@ -0,0 +1,29 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новая страница");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "page",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => ""
+	)
+);?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "page",
+		"AREA_FILE_SUFFIX" => "feature2",
+		"EDIT_TEMPLATE" => ""
+	)
+);?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "page",
+		"AREA_FILE_SUFFIX" => "feature1",
+		"EDIT_TEMPLATE" => ""
+	)
+);?><br>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
